0ccf9ee6e61a9315e07b33e1ce862d47fed5f31e
deepak goyal <deepak4669@hotmail.com>
2020-06-27 20:31:36 +0530

    transformer-greedy

--- run.py
+++ run.py
@@ -1,19 +1,32 @@
 import sys
+import torch
 
 arguments=sys.argv
 del arguments[0:1]
 
 action,model,decoding_algo=arguments
 
-from preprocessing.cornell-movie import get_vocabulary
+from Preprocessing.cornell_movie import get_vocabulary
 
+
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 voc=get_vocabulary()
 
 if action=='interact':
-    working_model=_
+    
     if model=='transformer':
         from Models.transformer import make_model
-        working_model=make_model
+        working_model=make_model(voc.num_words,voc.num_words,2,256,512,8,0.1)
+        working_model.to(device)
+        print(working_model)
+
+        from interactive_model import interact
+
+        interact(working_model,"greedy",voc)
+
+
+
+
 
 
 
