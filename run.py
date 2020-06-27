98e7ff6fe9e47a70534d5d5703812d2050a582b1
deepak goyal <deepak4669@hotmail.com>
2020-06-28 01:00:43 +0530

    transformer->decoding all

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import torch
 arguments=sys.argv
 del arguments[0:1]
 
-action,model,decoding_algo=arguments
+action,model,decoding_algo,decoding_parameter=arguments
 
 from Preprocessing.cornell_movie import get_vocabulary
 
@@ -22,7 +22,16 @@ if action=='interact':
 
         from interactive_model import interact
 
-        interact(working_model,"greedy",voc)
+        interact(working_model,decoding_algo,float(decoding_parameter),voc)
+    
+    # if model=='gru':
+
+    #     from Models.seq2seq_attn import make_model
+    #     working_model=make_model(voc.num_words,500,2,2,0.1,0.1,'dot')
+    #     working_model.to(device)
+    #     from interactive_model import interact
+    #     interact
+
 
 
 
